6f4386349baa193182863da9aa83d88d41f0b092
Alfred <arwagaju@gmail.com>
2020-06-23 09:56:29 -0400

    added a class that stores all the data

---
+++ storeLib/Data.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogicLib
+{
+    class Data
+    {
+
+        public List<Customer> Customers { get; }
+
+
+        public List<Store> Stores { get; }
+
+        public Data()
+        {
+            Product p1 = new Product("milk");
+            Product p2 = new Product("bananas");
+            Product p3 = new Product("brocolli");
+            Product p4 = new Product("yogurt");
+
+            //a list of inventory
+            List<Product> inventory1 = new List<Product>();
+            inventory1.Add(p1);
+            inventory1.Add(p2);
+            inventory1.Add(p3);
+            inventory1.Add(p4);
+
+            Store Walmart = new Store("New York", inventory1);
+
+            //meat prodcuts
+            Product p5 = new Product("liver");
+            Product p6 = new Product("fish");
+            Product p7 = new Product("beef");
+            Product p8 = new Product("chicken");
+
+            List<Product> inventory2 = new List<Product>();
+            inventory2.Add(p5);
+            inventory2.Add(p6);
+            inventory2.Add(p7);
+            inventory2.Add(p8);
+
+            Store Kroger = new Store("London", inventory2);
+
+            //adding stores to the storeList
+            Stores.Add(Walmart);
+            Stores.Add(Kroger);
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+    }
+}
