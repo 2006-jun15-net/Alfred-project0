0fa69c3455cafdb937147de71e063acbdd0b87bc
Alfred <arwagaju@gmail.com>
2020-06-27 17:16:22 -0400

    added a serialization method to convert class objects to JSON

--- ConsoleApp/ConsoleApp/Functionality.cs
+++ ConsoleApp/ConsoleApp/Functionality.cs
@@ -3,23 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+
 namespace ClassLibraryApp
 {
-    class Functionality
+   
+    public class Functionality
     {
+        public static List<Customer> Customers = new List<Customer>();
+
         public Functionality()
         {
-
+           
         }
+        
+       
+
+        
+        
 
         /// <summary>
         /// This method adds a new customer in the system
         /// </summary>
+        
+        
 
-        Data data = new Data();  // a data object to acccess the Customer list
+        //List<Customer> Customers = new List<Customer>();
+        
         public void addNewCustomer()
         {
-            
+
             while (true)
             {
                 Console.WriteLine("Enter 1 to add new customer, 0 to stopping adding");
@@ -39,7 +51,7 @@ namespace ClassLibraryApp
 
                     //adding customers in the storage, if the customer is not in the system, go ahead and add them, otherwise they
                     //are already in the system.
-                    data.Customers.Add(customer);
+                    Customers.Add(customer);
                     /*
                     if (searchCustomer(customer) == false)
                    {
@@ -62,34 +74,40 @@ namespace ClassLibraryApp
                     break;
                 }
 
-            }
-            
+            }Console.WriteLine($"The current number of customers is {Customers.Count}");
+            Customers.ForEach(customer => Console.WriteLine(customer));
+
 
             //data.Customers.ForEach(customer => Console.WriteLine(customer.LastName));
 
 
         }
+        
+
 
         /// <summary>
         /// This methods search a customer in the system. Returns true if the customer in the system otherwise false.
         /// </summary>
 
-        public  void searchCustomer()
+        
+        public bool searchCustomer()
         {
-           
+
             Console.WriteLine("Enter customer's first name");
             string inputfirstName = Console.ReadLine();  // first name
             Console.WriteLine("Enter customer's last name");
             string inputLastName = Console.ReadLine();   // last name
-            foreach (Customer cust in data.Customers)
+            foreach (Customer cust in Customers)
             {
                 if (inputfirstName.Equals(cust.FirstName) && inputLastName.Equals(cust.LastName))
                 {
                     Console.WriteLine($"{inputfirstName} {inputLastName} is in the system");
+                    return true;
 
                 }
 
-            }
+            }Console.WriteLine($"{inputfirstName} {inputLastName} is not in the system");
+            return false;
 
         }
 
@@ -97,7 +115,8 @@ namespace ClassLibraryApp
         /// <summary>
         /// This method places the order
         /// </summary>
-        public  void placeOrder()
+       
+        public void placeOrder()
         {
             Console.WriteLine("Select a store from the options below:");
             Console.WriteLine();
@@ -146,43 +165,33 @@ namespace ClassLibraryApp
                                     //removing product from the store after being selected
                                     //placeOrder = true
 
-                if(cart.Count == 50)  //limit quantity of products that the cart should have.
+                if (cart.Count == 50)  //limit quantity of products that the cart should have.
                 {
                     Console.WriteLine("Reached limit. Press 1 to remove a product");
                     int input = int.Parse(Console.ReadLine());
 
                     Console.WriteLine("Enter a product to remove:");
                     string prod = Console.ReadLine();
-                    if(input == 1)
+                    if (input == 1)
                     {
-                        foreach(Product cartprod in cart.ToList())
+                        foreach (Product cartprod in cart.ToList())
                         {
-                            if(cartprod.Name.Equals(prod))
+                            if (cartprod.Name.Equals(prod))
                             {
                                 cart.Remove(cartprod);
-                               
-                            }break; 
+
+                            }
+                            break;
                         }
 
-                        
+
 
                     }
-                    
+
                     break;
 
                 }
-                //removing selected products from the selected store
-                foreach (Store store in data.Stores)
-                {
-                    if (selectedStore.Equals(store.Name))
-                    {
-                        store.remove(product);
-                        
-                        
-                    }
-                    
 
-                }
                 //Console.WriteLine(store.Inventory.Count);
 
 
@@ -192,8 +201,8 @@ namespace ClassLibraryApp
                     break;
                 }
 
-               
-                    
+
+
 
 
 
@@ -212,7 +221,8 @@ namespace ClassLibraryApp
         }
 
         //displaying store locations
-        public  void display()
+        
+        public void display()
         {
             Data data = new Data();
             foreach (Store store in data.Stores)
@@ -224,14 +234,20 @@ namespace ClassLibraryApp
 
         }
 
-        public void displayOrders()
+         
+
+
+        /*public void displayOrders()
         {
             Console.WriteLine("Enter customer's first and last name");
             string firstName = Console.ReadLine();
             string lastName = Console.ReadLine();
             Customer customer = new Customer(firstName, lastName);
             customer.display();
-        }
+        }*/
+        
+
+
 
     }
 
--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using Newtonsoft.Json;
 
 
 
@@ -7,27 +10,30 @@ namespace ClassLibraryApp
 {
     class Program
     {
-     
+
 
         static void Main(string[] args)
         {
 
-            Functionality func = new Functionality();
-            func.displayOrders();
+            serialize(); //serialize data from Data class
+
+            deserialize();
             
-           /* Functionality functionality = new Functionality();
+
+
+            Functionality functionality = new Functionality();
 
             Console.WriteLine("welcome to the store application");
             bool choice = false;
             while (!choice)
             {
-               
+
                 Console.WriteLine("Press:\n 1 to add new customer\n 2 to place an order\n 3 to search a customer" +
                                    "\n 4 to quit the store system");
                 string option = Console.ReadLine();
                 int num = int.Parse(option);
 
-                switch(num)
+                switch (num)
                 {
                     case 1:
                         functionality.addNewCustomer();
@@ -41,29 +47,57 @@ namespace ClassLibraryApp
                         functionality.searchCustomer();
                         break;
 
+                   
                     case 4:
-                        functionality.displayOrders();
-                        break;
-
-
-
-                    case 5:
                         choice = true; //quiting from the store application
                         break;
-                 
+
 
 
 
                 }
-           */
+
 
 
             }
-           
+
+        }
+
+
+        /// <summary>
+        /// This method serializes hardcode data from Data class.
+        /// </summary>
+        /// 
+        const string filepath = @"../../../data.json"; //This string holds the filepath to which serialized data will be saved.
+        public static void serialize()
+        {
+            Data data = new Data();  //object to serilaze
+            Functionality functionality = new Functionality();
+
+            
+            string json = JsonConvert.SerializeObject(functionality);
+            File.WriteAllText(filepath, json);
+
+            string json1 = JsonConvert.SerializeObject(data);
+            File.WriteAllText(filepath, json1);
+
+        }
+
+        public static void deserialize()
+        {
+            Data data = new Data();  //object to deserilaze
+            Functionality functionality = new Functionality();
+            string json1 = JsonConvert.SerializeObject(data);
+
+            Object obj = JsonConvert.DeserializeObject<Object>(json1);
+
         }
 
 
 
-      
+       
+
+
     }
+}
 
