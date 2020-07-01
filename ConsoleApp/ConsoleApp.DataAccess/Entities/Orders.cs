6c434da3c63b5ad42d8ed2221d7bd1d96b43f75e
Alfred <arwagaju@gmail.com>
2020-07-01 05:32:12 -0400

    modified Order.cs file

--- ConsoleApp/ConsoleApp.DataAccess/Entities/Orders.cs
+++ ConsoleApp/ConsoleApp.DataAccess/Entities/Orders.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
 
 namespace ConsoleApp.DataAccess.Entities
 {
+    
     public partial class Orders
     {
+
+        [Key]
         public int OrderId { get; set; }
         public int CustId { get; set; }
 
